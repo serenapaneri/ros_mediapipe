c7fb4f39351a78f10201e28b84aa395088c5f5dd
root <serenapaneri2605988@gmail.com>
2022-10-14 09:07:24 +0000

    update

--- scripts/prova4.py
+++ scripts/prova4.py
@@ -20,60 +20,61 @@ sys.path.append('/home/spot/cv_bridge_ws/install/lib/python3/dist-packages') # c
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
-class mediapipe:
+cv_image = None
 
-    def __init__(self):
-        # setting the cv_bridge
-        self.bridge = CvBridge()
+def camera_callback(data):
+    """
+      Callback function of the spot camera
+    """
+    global cv_image
+    cv_image = data
 
-        # subscribers to the camera topic
-        self.camera_sub = rospy.Subscriber("/spot/camera/frontleft/image", Image, self.camera_callback)
-        
+def holistic():
+    global cv_image
+    rospy.init_node("holistic", anonymous = True)
 
-    def camera_callback(self, data):
-        """
-          Callback function of the rgb camera 
-        """
-        # setting mediapipe pose model
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_holistic = mp.solutions.holistic
+    # subscriber to the camera topic
+    camera_sub = rospy.Subscriber("/spot/camera/frontleft/image", Image, camera_callback)
 
-        try:
-            self.cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
-            # rotate the image of 90°
-            self.rot_image = cv2.rotate(cv_image, cv2.ROTATE_90_CLOCKWISE)
-        except CvBridgeError as e:
-            print(e)
+    # setting the cv_bridge
+    bridge = CvBridge()
 
-        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-            # in order to work with mediapipe we need the format RGB
-            self.rgb_image = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
-            rgb_image.flags.writeable = False
+    # setting mediapipe holistic model
+    mp_drawing = mp.solutions.drawing_utils
+    mp_holistic = mp.solutions.holistic
 
-            # make detections
-            self.results = holistic.process(rgb_image)
+    # converting the sensor image to a cv image
+    try:
+        image = bridge.imgmsg_to_cv2(cv_image, desired_encoding = 'bgr8')
+        rot_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+    except CvBridgeError as e:
+        print(e)
 
-            # in order to work with opencv we need the BGR format
-            rgb_image.flags.writeable = True
-            rgb_image = cv2.cvtColor(data, cv2.COLOR_RGB2BGR)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        # in order to work with mediapipe we need the format RGB
+        rgb_image = cv2.cvtColor(rot_image, cv2.COLOR_BGR2RGB)
+        rgb_image.flags.writeable = False
 
-            self.mp_drawing.draw_landmarks(rgb_image, results.face_landmarks, self.mp_holistic.FACE_CONNECTIONS,
+        # make detections
+        results = holistic.process(rgb_image)
+
+        # in order to work with opencv we need the BGR format
+        rgb_image.flags.writeable = True
+        rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+
+        mp_drawing.draw_landmarks(rgb_image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
                                      mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
                                      mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
                                      )
 
-            # body pose model
-            self.mp_drawing.draw_landmarks(rgb_image, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS,
+         # body pose model
+         mp_drawing.draw_landmarks(rgb_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                                      mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
                                      mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                                      )
 
-            cv2.imshow("mediapipe_image", rgb_image)
-            cv2.waitKey(3)
-
-def main(args):
-    rospy.init_node("holistic", anonymous = True)
-    medpipe = mediapipe()
+         cv2.imshow("mediapipe_image", rgb_image)
+         cv2.waitKey(3)
 
     try:
         rospy.spin()
@@ -81,5 +82,6 @@ def main(args):
         print("Shutting down")
     cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
-    main(sys.argv)
+    holistic()
