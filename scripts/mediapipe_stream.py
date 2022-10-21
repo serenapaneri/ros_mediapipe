6673968d849bb9f476834a43b7ae78fb0d1388ca
root <serenapaneri2605988@gmail.com>
2022-10-21 07:33:03 +0000

    update

--- scripts/mediapipe_stream.py
+++ scripts/mediapipe_stream.py
@@ -19,6 +19,12 @@ sys.path.append('/home/spot/cv_bridge_ws/install/lib/python3/dist-packages')
 
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from matplotlib.animation import FuncAnimation
+
+# global variables
+mpipe = None
+landmarks = None
+result = None # check this che mmmmmmhh
 
 class mediapipe:
 
@@ -41,22 +47,27 @@ class mediapipe:
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
             # rotate the image of 90°
-            rot_image = cv2.rotate(cv_image, cv2.ROTATE_90_CLOCKWISE) # cambia questo
+            # rot_image = cv2.rotate(cv_image, cv2.ROTATE_90_CLOCKWISE) # cambia questo
         except CvBridgeError as e:
             print(e)
 
         with self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             # in order to work with mediapipe we need the format RGB
-            rgb_image = cv2.cvtColor(rot_image, cv2.COLOR_BGR2RGB)
+            rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
             rgb_image.flags.writeable = False
 
             # make detections
             results = holistic.process(rgb_image)
+            if result.pose_landmarks:
+                mpipe = True
+            else:
+                mpipe = False
 
             # in order to work with opencv we need the BGR format
             rgb_image.flags.writeable = True
             rgb_image = cv2.cvtColor(data, cv2.COLOR_RGB2BGR)
 
+            # face mesh model
             self.mp_drawing.draw_landmarks(rgb_image, results.face_landmarks, self.mp_holistic.FACE_CONNECTIONS,
                                      self.mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
                                      self.mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
@@ -73,10 +84,30 @@ class mediapipe:
             cv2.imshow("mediapipe_image", rgb_image)
             cv2.waitKey(3)
 
+
+def calculate_angle(a, b, c):
+    """
+      Function used to compute an angle between 3 joints of interest.
+    """
+    a = np.array(a) # first joint
+    b = np.array(b) # mid joint
+    c = np.array(c) # end joint
+    
+    # calculate the radians between 3 joints and then convert it in angles
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    # the angle should be beween 0 and 180
+    if angle > 180.0:
+        angle = 360 - angle
+    return angle
+
+
 def main(args):
     rospy.init_node("mediapipe_stream", anonymous = True)
     medpipe = mediapipe()
-    print(medpipe)
+
+    landmarks = result.pose_landmarks.landmark # check this che mmmmhhhh
 
     try:
         rospy.spin()
