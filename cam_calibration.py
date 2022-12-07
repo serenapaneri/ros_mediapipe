616f5d5ffce27d02b99a8d61c07937b6998a1fcf
root <serenapaneri2605988@gmail.com>
2022-12-07 10:21:03 +0000

    update

--- cam_calibration.py
+++ cam_calibration.py
@@ -2,9 +2,9 @@
 import numpy as np
 import cv2 as cv
 import glob
+import yaml
 
 chessboardSize = (8,6)
-frameSize = (640,480)
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -13,82 +13,59 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
 
-size_of_chessboard_squares_mm = 25
-objp = objp * size_of_chessboard_squares_mm
-
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-
 images = glob.glob('*.jpg')
+# print(len(images))
+found = 0
+discarded = 0
 
 for image in images:
-
     img = cv.imread(image)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
-
+        print(image)
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
-
+        imgpoints.append(corners2)
         # Draw and display the corners
         cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        found += 1
         cv.imshow('img', img)
         cv.waitKey(1000)
+    else:
+        discarded += 1
 
+print(found)
+# print(discarded)
 cv.destroyAllWindows()
 
 ############## CALIBRATION #######################################################
 
-ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
-print("Camera Calibrated: ")
-print(ret)
-print("Camera Matrix:")
-print(cameraMatrix)
-print("Distortion Parameters:")
-print(dist)
-print("Rotation Vectors:")
-print(rvecs)
-print("Translation Vectors:")
-print(tvecs)
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-############## UNDISTORTION #####################################################
+# transform the matrix and distortion coefficients to writable lists
+data = {'camera_matrix': np.asarray(mtx).tolist(),
+        'dist_coeff': np.asarray(dist).tolist()}
 
-img = cv.imread('39.jpg')
-h,  w = img.shape[:2]
-newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+# and save it to a file
+with open("calibration_matrix.yaml", "w") as f:
+    yaml.dump(data, f)
 
-# Undistort
-dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
-
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('res39.jpg', dst)
-
-# Undistort with Remapping
-mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
-dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('res40.jpg', dst)
+# done
 
 # Reprojection Error
 mean_error = 0
 
 for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
     mean_error += error
 
 print( "total error: {}".format(mean_error/len(objpoints)) )
-
