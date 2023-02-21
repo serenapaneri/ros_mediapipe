fcc1ee25923f9b8d4f8b625f0e694148e3d1f1f0
root <serenapaneri2605988@gmail.com>
2023-02-21 11:36:13 +0000

    pointcloud

--- scripts/mediapipe_stream.py
+++ scripts/mediapipe_stream.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
-
 import rospy
 import cv2
 import mediapipe as mp
 import numpy as np
-import matplotlib.pyplot as plt
 import cv_bridge
-import yaml
 import time
 import os
-import math
 import sys
 import sensor_msgs.point_cloud2 as pc2
-from visualization_msgs.msg import Marker, MarkerArray
 
 # needed to use cv_bridge with python 3
 sys.path.remove('/opt/ros/melodic/lib/python2.7/dist-packages')
@@ -38,134 +32,80 @@ class mediapipe:
 
         # mediapipe variable for detection
         self.results = None
-        self.landmarks = []
 
-        # markers
-        self.spheres = Marker()
-        self.linelist = Marker()
-        self.marker_array = MarkerArray()
+        self.rgb_image = None
+
+        self.landmarks = []
+        # self.visibilities = []
+
+        # real coordinates from point_cloud2
+        self.x_coord = []
+        self.y_coord = []
+        self.z_coord = []
 
         # camera subscriber
         self.camera_sub = rospy.Subscriber("/kinect2/hd/image_color", Image, self.camera_callback)
         # point cloud subscriber
         self.camera_sub = rospy.Subscriber("/kinect2/hd/points", pc2.PointCloud2, self.cloud_callback)
-        # marker publisher
-        self.marker_pub = rospy.Publisher('markers', MarkerArray, queue_size = 10)
 
 
     def camera_callback(self, data):
 
-        try:
-            
+        try:            
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except CvBridgeError as e:
             print(e)
 
         with self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             # in order to work with mediapipe we need the format RGB
-            rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-            rgb_image.flags.writeable = False
+            self.rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            self.rgb_image.flags.writeable = False
 
-            # make detections
-            self.results = holistic.process(rgb_image)
+            ### make detections ###
+            self.results = holistic.process(self.rgb_image)
 
             # in order to work with opencv we need the BGR format
-            rgb_image.flags.writeable = True
-            rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+            self.rgb_image.flags.writeable = True
+            self.rgb_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_RGB2BGR)
 
-            # face mask model 
-            self.mp_drawing.draw_landmarks(rgb_image, self.results.face_landmarks, self.mp_holistic.FACE_CONNECTIONS,
+            ### face mask model ### 
+            self.mp_drawing.draw_landmarks(self.rgb_image, self.results.face_landmarks, self.mp_holistic.FACE_CONNECTIONS,
                                      self.mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
                                      self.mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
                                      )
 
-            # body pose model
-            self.mp_drawing.draw_landmarks(rgb_image, self.results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS,
+            ### body pose model ###
+            self.mp_drawing.draw_landmarks(self.rgb_image, self.results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS,
                                      self.mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
                                      self.mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                                      )
 
-            if self.results is not None and self.results.pose_landmarks:
-                pts = self.results.pose_landmarks.landmark
-                for id_,lm in enumerate(pts):
-                    h, w, c = rgb_image.shape
-                    imgx,imgy=int(lm.x*w),int(lm.y*h)
-                    self.landmarks.append((imgx,imgy))
 
-            cv2.imshow("mediapipe_image", rgb_image)
+            cv2.imshow("mediapipe_image", self.rgb_image)
             cv2.waitKey(3)
 
 
     def cloud_callback(self, point_data):
-        self.create_linelist(point_data)
-        self.create_spheres(point_data)
-        self.marker_array.markers.extend([self.spheres, self.linelist])
-        self.marker_pub.publish(self.marker_array)
+        
+        assert isinstance(point_data, PointCloud2)
+        ### converting the marker in pixels ###
+        if self.results is not None and self.results.pose_landmarks:
+            pts = self.results.pose_landmarks.landmark
 
+            # taking the nose coordinates
+            nose = pts[-33]
+            h, w, c = self.rgb_image.shape
+            imgx, imgy= int(nose.x*w), int(nose.y*h)
 
+            ### converting pixels into x,y,z point cloud ###
+            points3D = list(pc2.read_points(point_data, field_names=('x', 'y', 'z'), skip_nans=True, uvs = [(imgx, imgy)]))
+            for p in points3D:
+                self.x_coord.append(p[0])
+                self.y_coord.append(p[1])
+                self.z_coord.append(p[2])
+                # print(self.z_coord)
+                # print('x: {}, y: {}, z: {}'.format(p[0], p[1], p[2]))
 
-    def create_linelist(self, point_data):
-
-        try:
-            body = [['shoulder_line', [self.landmarks[11], self.landmarks[12]]], ['waist_line', [self.landmarks[23], self.landmarks[24]]], ['left_shoulder_waist', [self.landmarks[11],  self.landmarks[23]]], ['right_shoulder_waist', [self.landmarks[12], self.landmarks[24]]], ['right_thigh', [self.landmarks[24], self.landmarks[26]]], ['left_thigh',[self.landmarks[23], self.landmarks[25]]], ['right_leg', [self.landmarks[26], self.landmarks[28]]], ['left_leg', [self.landmarks[25], self.landmarks[27]]], ['right_forearm', [self.landmarks[14], self.landmarks[16]]], ['left_forearm', [self.landmarks[13], self.landmarks[15]]], ['right_bicep', [self.landmarks[12], self.landmarks[14]]], ['left_bicep', [self.landmarks[11], self.landmarks[13]]]]
-
-            self.linelist.points = []
-            # self.linelist.header.frame_id = "kinect2_rgb_optical_frame"
-            self.linelist.header.frame_id = "odom"
-            self.linelist.header.stamp = rospy.Time.now()
-            self.linelist.type = Marker.LINE_LIST
-
-            self.linelist.id = 1
-            self.linelist.action = Marker.ADD
-            self.linelist.scale.x = 0.05
-
-            self.linelist.color.g = 1.0
-            self.linelist.color.a = 1.0
-
-            for _,point1 in body:
-                for pt in point1:
-                   ptl_x, ptl_y, ptl_z = list(pc2.read_points(point_data, skip_nans=True, field_names = ('x', 'y', 'z'), uvs = [(pt[0], pt[1])]))[0]
-
-                   self.linelist_point = Point()
-                   self.linelist_point.x = ptl_x
-                   self.linelist_point.y = ptl_y
-                   self.linelist_point.z = ptl_z
-                   self.linelist.points.append(self.linelist_point)
-        except:
-            pass
-
-
-    def create_spheres(self, point_data):
-
-        try:
-            points = [self.landmarks[0], self.landmarks[15], self.landmarks[13], self.landmarks[11], self.landmarks[16], self.landmarks[14], self.landmarks[12], self.landmarks[23], self.landmarks[25], self.landmarks[27], self.landmarks[24], self.landmarks[26], self.landmarks[28]]
-
-            self.spheres.points = []
-            # self.spheres.header.frame_id = "kinect2_rgb_optical_frame"
-            self.spheres.header.frame_id = "odom"
-            self.spheres.header.stamp = rospy.Time.now()
-
-            self.spheres.id = 0
-            self.spheres.action = Marker.ADD
-
-            self.spheres.type = Marker.SPHERE_LIST
-            self.spheres.color.r = 1.0
-            self.spheres.color.a = 1.0
-
-            self.spheres.scale.x = 0.08
-            self.spheres.scale.y = 0.08
-            self.spheres.scale.z = 0.01
-
-            for p in points:
-               pts_x, pts_y, pts_z = list(pc2.read_points(point_data, skip_nans=True, field_names = ('x', 'y', 'z'), uvs = [(pt[0], pt[1])]))[0]
-
-               self.spheres_point = Point()
-               self.spheres_point.x = pts_x
-               self.spheres_point.y = pts_y
-               self.spheres_point.z = pts_z
-               self.spheres.points.append(self.spheres_point)
-        except:
-            pass
 
 
 
